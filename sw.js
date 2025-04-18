c3c39c15eb80905ccc07d7df7d19ff1eadf08d66
Zuri Ahola <zag924@gmail.com>
2025-04-17 21:45:39 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'customer-mgr-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/app.js',
+  '/assets/js/db.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
